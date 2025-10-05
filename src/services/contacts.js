6597c3100c4f263cdd61058cfc5f6566f12a3f23
Vika-Lex <148656797+Vika-Lex@users.noreply.github.com>
2025-10-05 09:33:24 +0300

    feat(contacts): add new files

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -10,7 +10,32 @@ export const getContactById = async (id) => {
   return contact;
 };
 
+export const createContact = async (contact) => {
+  const newContact = await ContactsCollection.create(contact);
+  return newContact;
+};
+
+export const updateContact = async (studentId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    {_id:studentId},
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options
+    }
+  );
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted)
+  };
+};
+
 export const deleteById = async (id) => {
-  const contact = await ContactsCollection.deleteOne(id);
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: id
+  });
   return contact;
 };
